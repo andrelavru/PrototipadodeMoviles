e1f0a4d9b4de59d1aaf32ba08ea1740477d5905e
andrelavru <andrelavru@gmail.com>
2016-04-05 15:35:27 -0500

    clase1y2

---
+++ Clase2/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerJump : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody2D body;
+	bool canJump;
+	Button jumpButton;
+	public float jumpPower = 12.0f;
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+		jumpButton = GameObject.Find("boton_salto").GetComponent<Button>();
+		jumpButton.onClick.AddListener (() => jump ());
+		canJump = false;
+
+	}
+	
+	// Update is called once per frame
+	void jump () {
+		if (canJump) {
+			canJump = false;
+			body.velocity = new Vector2(body.velocity.x, jumpPower);
+		}
+			
+	}
+
+	void Update () {
+		if (Mathf.Abs (body.velocity.y) == 0) {
+			canJump = true;
+		}
+	}
+}
