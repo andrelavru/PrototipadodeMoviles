f5e7d6aeda65a5756d1ecdd7f6751c58d40d92da
Alumno-J <Alumno-J>
2016-04-05 21:27:48 -0500

    clase3

---
+++ Clase3/Clase3/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerJump : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody2D body;
+	bool canJump;
+	Button jumpButton;
+	Animator anim;
+	public float jumpPower = 12.0f;
+	const string RUNNING = "running";
+	const string JUMPING = "jumping";
+	const string IDLE = "idle";
+
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+		jumpButton = GameObject.Find("boton_salto").GetComponent<Button>();
+		jumpButton.onClick.AddListener (() => jump ());
+		anim = GetComponent<Animator> ();
+		anim.Play (RUNNING);
+		canJump = false;
+
+	}
+	
+	// Update is called once per frame
+	void jump () {
+		if (canJump) {
+			canJump = false;
+			anim.Play (JUMPING);
+			body.velocity = new Vector2(body.velocity.x, jumpPower);
+		}
+			
+	}
+
+	void Update () {
+		if (Mathf.Abs (body.velocity.y) == 0) {
+			canJump = true;
+			anim.Play (RUNNING);
+		}
+	}
+}
---
+++ Clase3/Clase3/Assets/Scripts/conta.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class conta : MonoBehaviour {
+
+	// Use this for initialization
+	public Text Conta;
+	float elapsed;
+	int score;
+
+	void Start () {
+		score = 0;
+		elapsed = 0;
+		Conta.text = score.ToString();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.deltaTime;
+		if(elapsed >= 1f){
+			elapsed = 0;
+			score++;
+			Conta.text = score.ToString ();
+		}
+	}
+}
