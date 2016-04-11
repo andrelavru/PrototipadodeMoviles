045423d2f8e81d39b7649f6b9db6ae0cb999c6d5
andrelavru <andrelavru@gmail.com>
2016-04-10 21:51:11 -0500

    lol

---
+++ Clase2/Assets/Scripts/StartButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	// Use this for initialization
+	Button start;
+	void Start () {
+		start = GetComponent<Button>();
+		start.onClick.AddListener(() => goGame());
+	}
+
+	void goGame(){
+		SceneManager.LoadScene (1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
